f7b770e0ab3dfc635eb78046e90d0bce9d263e71
Jake Faulkner <jakefaulkn@gmail.com>
2017-04-15 16:48:43 +1200

    Write unit tests for classifier

---
+++ test/test_classifier.py
@@ -0,0 +1,56 @@
+from engine import classifier
+from enum import Enum
+
+
+class Class(Enum):
+    BANNANA = 0
+    APPLE = 1
+    ORANGE = 2
+
+
+TEST_DATASET = {
+    Class.BANNANA: [["long", "yellow", "sweet"],
+                    ["long", "bruised", "yellow", "sweet"],
+                    ["long", "yellow", "sweet"],
+                    ["long", "bruised", "yellow", "ripe"],
+                    ["yellow", "sweet", "rotten"]],
+    Class.APPLE: [["round", "bruised", "red"],
+                  ["round", "red", "sweet", "juicy"],
+                  ["rotten", "round", "green", "sweet"],
+                  ["round", "green", "sour"]],
+    Class.ORANGE: [["round", "orange", "juicy", "sour"],
+                   ["round", "ripe", "orange"],
+                   ["round", "orange", "sweet"],
+                   ["round", "orange", "sour"]]
+}.items()
+# Size: 13
+# P(B) = 5/13
+# P(A) = 4/13
+# P(O) = 4/13
+
+
+def test_group_class():
+    test_arr = [
+        [1, "green", "blue"],
+        [1, "red", "yellow"],
+        [2, "orange", "blue"]
+    ]
+    assert classifier.group_by_class(test_arr) == {
+        1: [["green", "blue"], ["red", "yellow"]],
+        2: [["orange", "blue"]]
+    }
+
+
+def test_classifier():
+    bclassifier = classifier.Classifier()
+    bclassifier.train(TEST_DATASET)
+    prediction, _ = bclassifier.classify(["round", "sour", "orange"])
+    assert prediction == Class.ORANGE
+    prediction, _ = bclassifier.classify(["bruised", "sweet"])
+    assert prediction == Class.BANNANA
+    prediction, _ = bclassifier.classify(["ripe"])
+    assert prediction == Class.BANNANA
+    prediction, _ = bclassifier.classify(["round", "sour"])
+    assert prediction == Class.ORANGE
+    prediction, _ = bclassifier.classify(["round", "sweet"])
+    assert prediction == Class.APPLE
