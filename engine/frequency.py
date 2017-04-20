ad04e9f99f2178bc0273a39fcc97e74a51806e91
Jake Faulkner <jakefaulkn@gmail.com>
2017-04-21 11:33:21 +1200

    Fix frequency tests and implementation

--- engine/frequency.py
+++ engine/frequency.py
@@ -3,9 +3,11 @@ the top 1000 most common words. P(r) is given by 0.1/r, where 1 < r < 1000.
 1000 words is the limit as beyond that the harmonic series diverges for less
 frequent words. About 50% of all words in the Oxford English Corpus reside in
 the top 100 words. """
+
+
 class Frequency():
     """ Contains the state for the frequency analysis. """
-    self.frequency_map = None
+    frequency_map = None
 
     def __init__(self, frequency_map):
         self.frequency_map = frequency_map
@@ -18,10 +20,11 @@ class Frequency():
             return None
         return 0.1 / (self.frequency_map[word])
 
-    def frequency_from(wordlist):
-        """ Return a frequency object from wordlist, a list of words ranked in
-        order according to their commonality. """
-        fmap = {}
-        for index, value in enumerate(wordlist):
-            fmap[value] = index + 1
-        return Frequency(fmap)
+
+def frequency_from(wordlist):
+    """ Return a frequency object from wordlist, a list of words ranked in
+    order according to their commonality. """
+    fmap = {}
+    for index, value in enumerate(wordlist):
+        fmap[value] = index + 1
+    return Frequency(fmap)
--- test/test_frequency.py
+++ test/test_frequency.py
@@ -8,4 +8,8 @@ FLIST = [
 ]
 
 def test_frequency_builder():
-    frequency = frequency.Frequency()
+    frqncy = frequency.frequency_from(FLIST)
+    assert frqncy.rank('a') == 0.1
+    assert frqncy.rank('b') == 0.05
+    assert frqncy.rank('c') == (0.1 / 3)
+    assert frqncy.rank('d') == (0.1 / 4)
