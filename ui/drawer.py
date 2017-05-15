e3eea4de58d24ab2298b87f08fbf4637faf47d9e
Jake Faulkner <jakefaulkn@gmail.com>
2017-05-15 16:24:33 +1200

    Swap to using dialog instead of slide out draw

--- ui/drawer.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QWidget
-
-class DrawerWidget(QWidget):
-    """ A slide out drawer widget. """
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
-        self.setGeometry(-self.width, 0, self.width, self.height)
-
-    def show(self):
