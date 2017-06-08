278bd702fd979be06d73509286272bcf95f1c948
Jake Faulkner <jakefaulkn@gmail.com>
2017-06-09 07:52:57 +1200

    Begin serialize implementation

--- db/serializer.py
+++ db/serializer.py
@@ -1,7 +1,26 @@
 import sqlite3
 
 class Serializable():
+
+    def __init__(self, serialize_table):
+        self.serialize_table = serialize_table
+
     def deserialize(self):
-        return None
-    def serialize(self):
-        return None
+        pass
+
+    def insert_into(self, sqlite_table):
+        ''' Serialize object into a sqlite INSERT call.
+        Example:
+        >>> class SerializeMe(Serializable):
+        >>>   def __init__(self, name):
+        >>>     self.name = name
+        >>>     self.seiralize_table = {'name': 'name'}
+        >>>
+        >>> test = SerializeMe('jack')
+        >>> query = test.insert_into('names')
+        >>> self.assertEquals(query, ('INSERT INTO names (name) VALUES (?)', 'jack')) '''
+        keys = list(self.serialize_table)
+        values = [getattr(self, prop) for prop in self.serialize_table.values()]
+        placeholders = ', '.join('?' for value in values)
+        sqlite_query_format = f'INSERT INTO {sqlite_table} ({keys}) VALUES ({placeholders})'
+        return sqlite_query_format, values
