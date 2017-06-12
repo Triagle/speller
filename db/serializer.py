00ae6e9462b92dfe111515b80aea70fd42856ccb
Jake Faulkner <jakefaulkn@gmail.com>
2017-06-12 17:16:43 +1200

    Implement deserialize_from; add documentation

--- db/serializer.py
+++ db/serializer.py
@@ -1,12 +1,29 @@
 import sqlite3
 
+
 class Serializable():
+    ''' Serializable class that implements methods for objects wishing to serialize to tables. '''
 
     def __init__(self, serialize_table):
+        ''' Initialize the Serializable class'''
         self.serialize_table = serialize_table
 
-    def deserialize(self):
-        pass
+    def deserialize_from(self, row):
+        ''' Deserialize object from the result of an sqlite SELECT call.
+        Example:
+
+        >>> class DeserializeMe(Serializable):
+        >>>   def __init__(self, name):
+        >>>     self.name = name
+        >>>     self.seiralize_table = {'name_column': 'name'}
+        >>>
+        >>> result = ['jake'] # SELECT name_column FROM names
+        >>> test_class = DeserializeMe(None)
+        >>> test_class.deserialize_from(result)
+        >>> assertEquals(test_class.name, 'jake') '''
+        deserialize_map = self.serialize_table.values()
+        for prop, value in zip(deserialize_map, row):
+            setattr(self, prop, value)
 
     def insert_into(self, sqlite_table):
         ''' Serialize object into a sqlite INSERT call.
@@ -18,8 +35,8 @@ class Serializable():
         >>>
         >>> test = SerializeMe('jack')
         >>> query = test.insert_into('names')
-        >>> self.assertEquals(query, ('INSERT INTO names (name) VALUES (?)', 'jack')) '''
-        keys = list(self.serialize_table)
+        >>> assertEquals(query, ('INSERT INTO names (name) VALUES (?)', 'jack')) '''
+        keys = ', '.join(list(self.serialize_table))
         values = [getattr(self, prop) for prop in self.serialize_table.values()]
         placeholders = ', '.join('?' for value in values)
         sqlite_query_format = f'INSERT INTO {sqlite_table} ({keys}) VALUES ({placeholders})'
