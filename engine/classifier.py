546ad55af8d2235c9b1f428e17137f14716174c2
Jake Faulkner <jakefaulkn@gmail.com>
2017-06-12 17:16:59 +1200

    Implement serialization for classifier

--- engine/classifier.py
+++ engine/classifier.py
@@ -1,10 +1,12 @@
 """ A naive bayesian classifier implementation that provides a general purpose,
 yet flexible method of classifying objects into classes. """
-class Type:
+
+from db import Serializable
+import json
+
+
+class Type(Serializable):
     """ Calculates the probability that an object belongs to a given class. """
-    cls = None
-    property_probability = {}
-    class_probability = 0
 
     def __init__(self, cls, class_probability):
         self.cls = cls
@@ -22,6 +24,13 @@ class Type:
                     self.property_probability[prop] = 1
         for prop, prob in self.property_probability.items():
             self.property_probability[prop] /= data_size
+
+    def as_json(self):
+        ''' Return a json friendly representation of Type class. '''
+        return {'__type__': True,
+                'class_probability': self.class_probability,
+                'properties': self.property_probability}
+
     def summarize(self):
         """ Return probabilities of both classes and properties. """
         return (self.class_probability, self.property_probability)
@@ -36,9 +45,18 @@ class Type:
                 return 0
         return probability
 
-class Classifier:
+
+class Classifier(Serializable):
     """ General purpose bayesian classifier. """
-    classes = []
+
+    def __init__(self, classes=[]):
+        ''' Initialize Classifier. '''
+        self.classes = []
+        serialize_table = {
+            'data': 'json_rep'
+        }
+        super().__init__(serialize_table)
+
     def train(self, dataset):
         """ Train the classifier from a dataset. """
         dataset_length = sum([len(v) for _, v in dataset])
@@ -46,6 +64,15 @@ class Classifier:
             cls_type = Type(cls, len(points) / dataset_length)
             cls_type.train(points)
             self.classes.append(cls_type)
+
+    @property
+    def json_rep(self):
+        return json.dumps(self, cls=ClassifierEncoder)
+
+    def as_json(self):
+        ''' Return a json friendly representation of Classifier'''
+        return {'__classifier__': True, 'classes': self.classes}
+
     def classify(self, properties):
         """ Classify an object into a class. """
         likely_cls = (None, 0)
@@ -55,6 +82,15 @@ class Classifier:
             if prob > max_prob:
                 likely_cls = (cls.cls, prob)
         return likely_cls
+
+
+class ClassifierEncoder(json.JSONEncoder):
+    ''' Encode Classifier and Properties to json format for database
+    insertion. '''
+    def default(self, obj):
+        if isinstance(obj, Classifier) or isinstance(obj, Type):
+            return obj.as_json()
+
 
 def group_by_class(dataset, cls_func=lambda x: (x[0], x[1:])):
     """ Group a list by it's class (or rather by function). e.g
