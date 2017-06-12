eac09c21c8f43234699ca81ca9e1cc717dd3d030
Jake Faulkner <jakefaulkn@gmail.com>
2017-06-13 11:29:26 +1200

    Lay groundwork for classifier powered spell check

--- ui/manager.py
+++ ui/manager.py
@@ -1,11 +1,29 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
 from ui import speller
 from ui import correction_dialog
+from db import words
+import sqlite3
 import sys
+
+DATABASE_LOCATION = 'db/data.db'
 
 
 class UIManager:
     """ Manage UI state. """
+    def get_editor_words(self):
+        text_editor_text = self.main_window_state.textEdit.toPlainText()
+        return text_editor_text.split(' ')
+
+    def get_unrecognized_words(self, candidates):
+        text_editor_words = self.get_text_editor_words()
+        cursor = self.conn.cursor()
+        return [word for word in text_editor_words
+                if not words.word_exists(cursor, word)]
+
+    def get_words(self):
+        cursor = self.conn.cursor()
+        return words.get_word_list(cursor)
+
     def show_dialog(self):
         """ Show corrections dialog """
         dialog = QDialog(self.main_window)
@@ -15,6 +33,10 @@ class UIManager:
     def connect_slots(self):
         """ Connect UI slots for various ui objects"""
         self.main_window_state.actionCorrections.triggered.connect(self.show_dialog)
+
+    def load_db(self):
+        cursor = self.conn.cursor()
+        self.classifier =
 
     def __init__(self):
         """ Setup UI objects. """
@@ -27,5 +49,8 @@ class UIManager:
 
     def run(self):
         """ Run UIManager. """
-        self.main_window.show()
-        return self.application.exec_()
+        with sqlite3.connect(DATABASE_LOCATION) as conn:
+            self.conn = conn
+            self.main_window.show()
+            res = self.application.exec_()
+        return res
